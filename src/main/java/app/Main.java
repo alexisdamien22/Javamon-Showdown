f41dfe2c9918593416023bb74d96d80f2cfd9052
Alexis DAMIEN <alexisdamien@icloud.com>
2026-03-13 14:11:34 +0100

    added base project

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,21 @@
+package app; 
+ 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene; 
+import javafx.stage.Stage;
+ 
+public class Main extends Application { 
+    @Override 
+    public void start(Stage stage) throws Exception { 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/layout.fxml")); 
+        Scene scene = new Scene(loader.load()); 
+        stage.setScene(scene); 
+        stage.setTitle("Mon Application JavaFX"); 
+        stage.show(); 
+    } 
+ 
+    public static void main(String[] args) { 
+        launch(); 
+    } 
+} 
